f94f59f2b67f03470a32893c5a839f80e3abac73
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2012-05-03 01:32:27 -0400

    testing location stuff

--- explore.rb
+++ explore.rb
@@ -3,14 +3,16 @@ require 'sinatra'
 require 'haml'
 require 'json'
 
+authorized = false
+
 get '/' do
-	cookie = nil
-	if cookie.nil?
-		# redirect to('/login')
-		print request
+	if request.params['code']
+		authorized = true
+		redirect to "/success?code=#{request.params['code']}"
 	end
-	# 'did not redirect'
-	request.inspect
+
+	redirect to '/login' unless authorized
+	"did not redirect<br/>" + request.inspect
 end
 
 get '/login' do
@@ -18,4 +20,14 @@ get '/login' do
 	request_url = 'https://foursquare.com/oauth2/access_token?client_id=EA2IBQTZ44BZZOFZSH4NRIK5NWNL0EINW3KOTTPFCYD4J4L2&client_secret=EJWCXQUCU01ZDWNVX1TTC34PQRUV5QK1O2G5Y34QU0UHEHPQ&grant_type=authorization_code&redirect_uri=http://vivekbhagwat.com&code=BYLUI4XNAN0NYSG40HCS1QVPLA5KHE1JSXULMB34JJACAO35'
 
 	haml :login, :locals => {:connect_url => url}
+end
+
+get '/success' do
+	redirect to '/login' unless authorized
+
+	#call python script, list results.
+end
+
+get '/test' do
+	haml :location
 end
