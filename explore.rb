964110111c484f899f65a731ccc52cc25f9cfa80
Vivek Bhagwat <bhagwat.vivek@gmail.com>
2012-05-02 21:33:07 -0400

    started sinatra app

---
+++ explore.rb
@@ -0,0 +1,19 @@
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+require 'json'
+
+get '/' do
+	cookie = nil
+	if cookie.nil?
+		redirect to('/login')
+	end
+	'did not redirect'
+end
+
+get '/login' do
+	access_url = 'https://foursquare.com/oauth2/authenticate?client_id=EA2IBQTZ44BZZOFZSH4NRIK5NWNL0EINW3KOTTPFCYD4J4L2&response_type=code&redirect_uri=http://vivekbhagwat.com'
+	request_url = 'https://foursquare.com/oauth2/access_token?client_id=EA2IBQTZ44BZZOFZSH4NRIK5NWNL0EINW3KOTTPFCYD4J4L2&client_secret=EJWCXQUCU01ZDWNVX1TTC34PQRUV5QK1O2G5Y34QU0UHEHPQ&grant_type=authorization_code&redirect_uri=http://vivekbhagwat.com&code=BYLUI4XNAN0NYSG40HCS1QVPLA5KHE1JSXULMB34JJACAO35'
+
+	haml :login, :locals => {:connect_url => url}
+end
